265caf0db1b1c8f5384425dd2c617c05e2ac8cbc
Rajan <rajanbhadauria@gmail.com>
2021-07-25 11:55:13 +0530

    Registration page component added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,23 +8,13 @@ import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from
 })
 export class RegisterComponent implements OnInit {
 
-  confirmEquals(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } | null =>  
-        control.value?.toLowerCase() === this.passwordValue.toLowerCase() 
-            ? null : {noMatch: true};
-  }
-
-  get passwordValue() {
-    return this.password.value;
-  }
-
-  signupForm = new FormGroup({
+    signupForm = new FormGroup({
     first_name: new FormControl('', Validators.required),
     last_name: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
-    confirm_password: new FormControl('', [Validators.required, Validators.confirmEquals])
-  }))
+    confirm_password: new FormControl('', Validators.required)
+  })
   constructor() { }
 
   ngOnInit(): void {
