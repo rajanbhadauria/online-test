3049daca3628e435264142b5f75abe23429e9fc3
rajanbhadauria <rajanbhadauria@gmail.com>
2021-07-31 11:12:58 +0530

    Register servive created

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-    signupForm = new FormGroup({
+  signupForm = new FormGroup({
     first_name: new FormControl('', Validators.required),
     last_name: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
-    confirm_password: new FormControl('', Validators.required)
-  })
-  constructor() { }
+    confirm_password: new FormControl('', Validators.required),
+  });
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
