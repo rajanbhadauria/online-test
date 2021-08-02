c189bf5919406ce67ec5e5216914b09db7454a2e
Rajan B <rajanbhadauria@gmail.com>
2021-08-02 20:01:16 +0530

    Registration service created

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -1,9 +1,19 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+
+   }
+  
+  saveUser(userData) {
+    this.http.post(this.apiUrl+"/users/signup", userData).
+  }
 }
